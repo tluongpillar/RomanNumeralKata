24d6eb4e7c7fc3ed2e0ac5614a5c9271ff223403
Thinh Luong <tluong@pillartechnology.com>
2016-08-17 22:20:09 -0500

    Added test for failure for libcheck

--- tests/check_libcheck.c
+++ tests/check_libcheck.c
@@ -1,8 +1,15 @@
+#include <stdlib.h>
 #include <check.h>
 
-START_TEST (test_libcheck)
+START_TEST (test_libcheck_one_equals_one__returns_true)
 {
-  ck_assert( 1 == 2);
+  ck_assert( 1 == 1);
+}
+END_TEST
+
+START_TEST (test_libcheck_one_equals_two__returns_false)
+{
+  ck_assert( 1 != 2);
 }
 END_TEST
 
@@ -14,7 +21,8 @@ Suite * libcheck_suite()
   suite = suite_create("Libcheck");
   tcase_core = tcase_create("Core");
 
-  tcase_add_test(tcase_core, test_libcheck);
+  tcase_add_test(tcase_core, test_libcheck_one_equals_one__returns_true);
+  tcase_add_test(tcase_core, test_libcheck_one_equals_two__returns_false);
   suite_add_tcase(suite, tcase_core);
 
   return suite;
@@ -22,6 +30,7 @@ Suite * libcheck_suite()
 
 int main()
 {
+  int failure_count;
   Suite *suite;
   SRunner *srunner;
 
@@ -29,7 +38,8 @@ int main()
   srunner = srunner_create(suite);
 
   srunner_run_all(srunner, CK_NORMAL);
+  failure_count = srunner_ntests_failed(srunner);
   srunner_free(srunner);
 
-  return 0;
+  return (failure_count == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
