2def3b106ce2353325f16d1c6c876312e0d69fb9
Thinh Luong <tluong@pillartechnology.com>
2016-08-18 21:36:04 -0500

    Updated test for all valid roman numerals

--- tests/check_roman_numeral.c
+++ tests/check_roman_numeral.c
@@ -8,7 +8,15 @@ END_TEST
 
 START_TEST (test_is_roman_numeral__given_valid_roman_numeral__returns_true)
 {
-  ck_assert_uint_eq(1, is_roman_numeral('I'));
+  char valid_roman_numerals[] = "IVXLCDM";
+
+  int valid_roman_numerals_count = sizeof(valid_roman_numerals) / sizeof(valid_roman_numerals[0]);
+
+  int index;
+  for (index = 0; index < valid_roman_numerals_count; ++index)
+  {
+    ck_assert_uint_eq(1, is_roman_numeral(valid_roman_numerals[index]));
+  }
 }
 END_TEST
 
