0d3a39d70dc6154017d741866d5b1ad279813cda
Thinh Luong <tluong@pillartechnology.com>
2016-08-18 21:39:09 -0500

    Passed test is_roman_numeral checking for valid roman numerals

--- src/roman_numeral.c
+++ src/roman_numeral.c
@@ -2,5 +2,17 @@
 
 uint is_roman_numeral(char roman_numeral)
 {
-  return (roman_numeral == 'I') ? 1 : 0;
+  char valid_roman_numerals[] = "IVXLCDM";
+  int valid_roman_numerals_count = sizeof(valid_roman_numerals) / sizeof(valid_roman_numerals[0]);
+
+  int index;
+  for (index = 0; index < valid_roman_numerals_count; ++index)
+  {
+    if (roman_numeral == valid_roman_numerals[index])
+    {
+      return 1;
+    }
+  }
+
+  return 0;
 }
