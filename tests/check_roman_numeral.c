6e1b2dc1ea59d429d55247091c35ad0db78f781c
Thinh Luong <tluong@pillartechnology.com>
2016-08-18 21:48:26 -0500

    Expanded is_roman_numeral test to test for lowercase input.

--- tests/check_roman_numeral.c
+++ tests/check_roman_numeral.c
@@ -9,13 +9,15 @@ END_TEST
 START_TEST (test_is_roman_numeral__given_valid_roman_numeral__returns_true)
 {
   char valid_roman_numerals[] = "IVXLCDM";
-
   int valid_roman_numerals_count = sizeof(valid_roman_numerals) / sizeof(valid_roman_numerals[0]);
 
   int index;
   for (index = 0; index < valid_roman_numerals_count; ++index)
   {
     ck_assert_uint_eq(1, is_roman_numeral(valid_roman_numerals[index]));
+
+    char valid_roman_numeral_lowercase = tolower(valid_roman_numerals[index]);
+    ck_assert_uint_eq(1, is_roman_numeral(valid_roman_numeral_lowercase));
   }
 }
 END_TEST
