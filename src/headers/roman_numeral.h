80ad92c74b96dad4ab381bf3543bb1b96541016b
Thinh Luong <tluong@pillartechnology.com>
2016-08-19 23:41:12 -0500

    Added ctype.h include to fixed implicit function warnings.

--- src/headers/roman_numeral.h
+++ src/headers/roman_numeral.h
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 bool is_roman_numeral(char roman_numeral);
 unsigned int get_arabic_value_from_roman_numeral(char *roman_numeral);
--- tests/headers/check_roman_numeral.h
+++ tests/headers/check_roman_numeral.h
@@ -1,6 +1,7 @@
 #include <check.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 #include "../../src/headers/roman_numeral.h"
 
 Suite * roman_numeral_suite();
