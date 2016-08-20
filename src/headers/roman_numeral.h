3f29b4a6bd22564c2aac26bc1d10f9fb43ef056d
Thinh Luong <tluong@pillartechnology.com>
2016-08-20 10:24:22 -0500

    Refactored case labels to use defined ROMAN_NUMERAL_LETTER_*

--- src/headers/roman_numeral.h
+++ src/headers/roman_numeral.h
@@ -3,5 +3,13 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+#define ROMAN_NUMERAL_LETTER_I 'I'
+#define ROMAN_NUMERAL_LETTER_V 'V'
+#define ROMAN_NUMERAL_LETTER_X 'X'
+#define ROMAN_NUMERAL_LETTER_L 'L'
+#define ROMAN_NUMERAL_LETTER_C 'C'
+#define ROMAN_NUMERAL_LETTER_D 'D'
+#define ROMAN_NUMERAL_LETTER_M 'M'
+
 bool is_roman_numeral(char roman_numeral);
 unsigned int get_arabic_value_from_roman_numeral(char *roman_numeral);
--- src/roman_numeral.c
+++ src/roman_numeral.c
@@ -4,13 +4,13 @@ bool is_roman_numeral(char roman_numeral)
 {
   switch (toupper(roman_numeral))
   {
-    case 'I':
-    case 'V':
-    case 'X':
-    case 'L':
-    case 'C':
-    case 'D':
-    case 'M':
+    case ROMAN_NUMERAL_LETTER_I:
+    case ROMAN_NUMERAL_LETTER_V:
+    case ROMAN_NUMERAL_LETTER_X:
+    case ROMAN_NUMERAL_LETTER_L:
+    case ROMAN_NUMERAL_LETTER_C:
+    case ROMAN_NUMERAL_LETTER_D:
+    case ROMAN_NUMERAL_LETTER_M:
       return true;
     default:
       return false;
@@ -23,25 +23,25 @@ unsigned int get_arabic_value_from_roman_numeral(char *roman_numeral)
 
   switch (*roman_numeral)
   {
-    case 'I':
+    case ROMAN_NUMERAL_LETTER_I:
       arabic_value = 1;
       break;
-    case 'V':
+    case ROMAN_NUMERAL_LETTER_V:
       arabic_value = 5;
       break;
-    case 'X':
+    case ROMAN_NUMERAL_LETTER_X:
       arabic_value = 10;
       break;
-    case 'L':
+    case ROMAN_NUMERAL_LETTER_L:
       arabic_value = 50;
       break;
-    case 'C':
+    case ROMAN_NUMERAL_LETTER_C:
       arabic_value = 100;
       break;
-    case 'D':
+    case ROMAN_NUMERAL_LETTER_D:
       arabic_value = 500;
       break;
-    case 'M':
+    case ROMAN_NUMERAL_LETTER_M:
       arabic_value = 1000;
       break;
     default:
