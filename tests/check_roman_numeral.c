b8f571241556e1dba623630ab8359eb670fe43f8
Thinh Luong <tluong@pillartechnology.com>
2016-08-20 11:08:52 -0500

    Consolidated get_arabic_value_from_roman_numeral multiple test cases into one test.

--- tests/check_roman_numeral.c
+++ tests/check_roman_numeral.c
@@ -1,5 +1,17 @@
 #include "headers/check_roman_numeral.h"
 
+START_TEST (test_get_arabic_value_from_roman_numeral__given_ROMAN_NUMERAL_LETTERs__returns_expected_arabic_values)
+{
+  ck_assert_uint_eq(1, get_arabic_value_from_roman_numeral(ROMAN_NUMERAL_LETTER_I));
+  ck_assert_uint_eq(5, get_arabic_value_from_roman_numeral(ROMAN_NUMERAL_LETTER_V));
+  ck_assert_uint_eq(10, get_arabic_value_from_roman_numeral(ROMAN_NUMERAL_LETTER_X));
+  ck_assert_uint_eq(50, get_arabic_value_from_roman_numeral(ROMAN_NUMERAL_LETTER_L));
+  ck_assert_uint_eq(100, get_arabic_value_from_roman_numeral(ROMAN_NUMERAL_LETTER_C));
+  ck_assert_uint_eq(500, get_arabic_value_from_roman_numeral(ROMAN_NUMERAL_LETTER_D));
+  ck_assert_uint_eq(1000, get_arabic_value_from_roman_numeral(ROMAN_NUMERAL_LETTER_M));
+}
+END_TEST
+
 START_TEST (test_get_arabic_value_from_roman_numeral__given_I__returns_1)
 {
   char roman_numeral = 'I';
@@ -87,6 +99,7 @@ Suite * roman_numeral_suite()
   suite = suite_create("RomanNumeral");
   tcase_core = tcase_create("Core");
 
+  tcase_add_test(tcase_core, test_get_arabic_value_from_roman_numeral__given_ROMAN_NUMERAL_LETTERs__returns_expected_arabic_values);
   tcase_add_test(tcase_core, test_get_arabic_value_from_roman_numeral__given_I__returns_1);
   tcase_add_test(tcase_core, test_get_arabic_value_from_roman_numeral__given_V__returns_5);
   tcase_add_test(tcase_core, test_get_arabic_value_from_roman_numeral__given_X__returns_10);
