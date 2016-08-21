0cd444b80184d47a0510f5a1d9159d0fad45ed2c
Thinh Luong <tluong@pillartechnology.com>
2016-08-21 18:48:25 -0500

    Passed test to convert roman numeral II to arabic value 2.

--- src/headers/roman_numeral_conversion.h
+++ src/headers/roman_numeral_conversion.h
@@ -1,3 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
+#include "roman_numeral_letter.h"
 
 unsigned int convert_to_arabic_value_from_roman_numeral(char roman_numeral[]);
--- src/roman_numeral_conversion.c
+++ src/roman_numeral_conversion.c
@@ -2,5 +2,18 @@
 
 unsigned int convert_to_arabic_value_from_roman_numeral(char roman_numeral[])
 {
-  return 0;
+  int arabic_value = 0;
+
+  int roman_numeral_count = strlen(roman_numeral);
+
+  int index;
+  for (index = 0; index < roman_numeral_count; ++index)
+  {
+    if (is_roman_numeral_letter(roman_numeral[index]))
+    {
+      arabic_value += get_arabic_value_from_roman_numeral_letter(roman_numeral[index]);
+    }
+  }
+
+  return arabic_value;
 }
