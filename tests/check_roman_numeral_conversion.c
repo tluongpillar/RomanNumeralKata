bb9cbae898f7e53aacc356e7dfd1fa1458162209
Thinh Luong <tluong@pillartechnology.com>
2016-08-21 21:34:00 -0500

    Added and passed test to convert roman numeral C-CM by C to arabic value 100-900 by 100.

--- tests/check_roman_numeral_conversion.c
+++ tests/check_roman_numeral_conversion.c
@@ -28,6 +28,20 @@ START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_X_through_XC_b
 }
 END_TEST
 
+START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_C_through_CM_by_C__returns_100_through_900_by_100)
+{
+  ck_assert_uint_eq(100, convert_to_arabic_value_from_roman_numeral("C"));
+  ck_assert_uint_eq(200, convert_to_arabic_value_from_roman_numeral("CC"));
+  ck_assert_uint_eq(300, convert_to_arabic_value_from_roman_numeral("CCC"));
+  ck_assert_uint_eq(400, convert_to_arabic_value_from_roman_numeral("CD"));
+  ck_assert_uint_eq(500, convert_to_arabic_value_from_roman_numeral("D"));
+  ck_assert_uint_eq(600, convert_to_arabic_value_from_roman_numeral("DC"));
+  ck_assert_uint_eq(700, convert_to_arabic_value_from_roman_numeral("DCC"));
+  ck_assert_uint_eq(800, convert_to_arabic_value_from_roman_numeral("DCCC"));
+  ck_assert_uint_eq(900, convert_to_arabic_value_from_roman_numeral("CM"));
+}
+END_TEST
+
 Suite * roman_numeral_conversion_suite()
 {
   Suite *suite;
@@ -36,6 +50,7 @@ Suite * roman_numeral_conversion_suite()
   suite = suite_create("RomanNumeralConversion");
   tcase_core = tcase_create("Core");
 
+  tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_C_through_CM_by_C__returns_100_through_900_by_100);
   tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_X_through_XC_by_X__returns_10_through_90_by_10);
   tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_I_through_IX__returns_1_through_9);
 
