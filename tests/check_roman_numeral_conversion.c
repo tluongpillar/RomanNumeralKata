56b11d1a1432a7ea9054d621216c4515276f19d7
Thinh Luong <tluong@pillartechnology.com>
2016-08-21 19:09:29 -0500

    Added and passed test to convert roman numeral I-IX to arabic value 1-9.

--- tests/check_roman_numeral_conversion.c
+++ tests/check_roman_numeral_conversion.c
@@ -1,5 +1,19 @@
 #include "headers/check_roman_numeral_conversion.h"
 
+START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_I_through_IX__returns_1_through_9)
+{
+  ck_assert_uint_eq(1, convert_to_arabic_value_from_roman_numeral("I"));
+  ck_assert_uint_eq(2, convert_to_arabic_value_from_roman_numeral("II"));
+  ck_assert_uint_eq(3, convert_to_arabic_value_from_roman_numeral("III"));
+  ck_assert_uint_eq(4, convert_to_arabic_value_from_roman_numeral("IV"));
+  ck_assert_uint_eq(5, convert_to_arabic_value_from_roman_numeral("V"));
+  ck_assert_uint_eq(6, convert_to_arabic_value_from_roman_numeral("VI"));
+  ck_assert_uint_eq(7, convert_to_arabic_value_from_roman_numeral("VII"));
+  ck_assert_uint_eq(8, convert_to_arabic_value_from_roman_numeral("VIII"));
+  ck_assert_uint_eq(9, convert_to_arabic_value_from_roman_numeral("IX"));
+}
+END_TEST
+
 START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_IX__returns_9)
 {
   char roman_numeral[] = "IX";
@@ -36,6 +50,7 @@ Suite * roman_numeral_conversion_suite()
   suite = suite_create("RomanNumeralConversion");
   tcase_core = tcase_create("Core");
 
+  tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_I_through_IX__returns_1_through_9);
   tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_IX__returns_9);
   tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_VIII__returns_8);
   tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_IV__returns_4);
