55d8360ebc564c462b32e7bf3dbc1cbc433ec1c2
Thinh Luong <tluong@pillartechnology.com>
2016-08-21 19:04:31 -0500

    Added and passed test to convert roman numeral VIII to arabic value 8.

--- tests/check_roman_numeral_conversion.c
+++ tests/check_roman_numeral_conversion.c
@@ -1,5 +1,12 @@
 #include "headers/check_roman_numeral_conversion.h"
 
+START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_VIII__returns_8)
+{
+  char roman_numeral[] = "VIII";
+  ck_assert_uint_eq(8, convert_to_arabic_value_from_roman_numeral(roman_numeral));
+}
+END_TEST
+
 START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_IV__returns_4)
 {
   char roman_numeral[] = "IV";
@@ -22,6 +29,7 @@ Suite * roman_numeral_conversion_suite()
   suite = suite_create("RomanNumeralConversion");
   tcase_core = tcase_create("Core");
 
+  tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_VIII__returns_8);
   tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_IV__returns_4);
   tcase_add_test(tcase_core, test_convert_to_arabic_value_from_roman_numeral__given_II__returns_2);
   suite_add_tcase(suite, tcase_core);
