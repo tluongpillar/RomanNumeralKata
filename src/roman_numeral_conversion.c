d0cb0368bde75904030aa0ba9ef072ce75548b97
Thinh Luong <tluong@pillartechnology.com>
2016-08-21 19:02:48 -0500

    Passed test to convert roman numeral IV to arabic value 4.

--- src/roman_numeral_conversion.c
+++ src/roman_numeral_conversion.c
@@ -7,11 +7,29 @@ unsigned int convert_to_arabic_value_from_roman_numeral(char roman_numeral[])
   int roman_numeral_count = strlen(roman_numeral);
 
   int index;
+  char current_roman_numeral_letter;
+  char next_roman_numeral_letter;
+  unsigned int current_roman_numeral_letter_arabic_value;
+  unsigned int next_roman_numeral_letter_arabic_value;
+
   for (index = 0; index < roman_numeral_count; ++index)
   {
-    if (is_roman_numeral_letter(roman_numeral[index]))
+    current_roman_numeral_letter = roman_numeral[index];
+
+    if (is_roman_numeral_letter(current_roman_numeral_letter))
     {
-      arabic_value += get_arabic_value_from_roman_numeral_letter(roman_numeral[index]);
+      next_roman_numeral_letter = roman_numeral[index + 1];
+
+      current_roman_numeral_letter_arabic_value = get_arabic_value_from_roman_numeral_letter(current_roman_numeral_letter);
+      next_roman_numeral_letter_arabic_value = get_arabic_value_from_roman_numeral_letter(next_roman_numeral_letter);
+
+      if (next_roman_numeral_letter_arabic_value > current_roman_numeral_letter_arabic_value)
+      {
+        arabic_value -= current_roman_numeral_letter_arabic_value;
+      } else
+      {
+        arabic_value += current_roman_numeral_letter_arabic_value;
+      }
     }
   }
 
