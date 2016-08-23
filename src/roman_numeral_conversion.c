94a8c549188c1a232e04954d6fc2ce31f05196e8
Thinh Luong <tluong@pillartechnology.com>
2016-08-22 21:17:24 -0500

    Passed test convert_to_roman_numeral_from_arabic_value given arabic value 1 to roman numeral I.

--- src/roman_numeral_conversion.c
+++ src/roman_numeral_conversion.c
@@ -2,7 +2,7 @@
 
 void convert_to_roman_numeral_from_arabic_value(char ** roman_numeral, unsigned int arabic_value)
 {
-  roman_numeral = NULL;
+  *roman_numeral = "I";
 }
 
 unsigned int convert_to_arabic_value_from_roman_numeral(char * roman_numeral)
