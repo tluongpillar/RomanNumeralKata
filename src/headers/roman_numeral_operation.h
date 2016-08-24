acaec1c190a65102d9eb3de191fac3dbf7b95052
Thinh Luong <tluong@pillartechnology.com>
2016-08-23 22:42:46 -0500

    Passed test add_two_roman_numerals given I and I equals II.

--- src/headers/roman_numeral_operation.h
+++ src/headers/roman_numeral_operation.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "roman_numeral_letter.h"
+#include "roman_numeral_conversion.h"
 
 bool add_two_roman_numerals(char ** result, char * lhs, char * rhs);
--- src/roman_numeral_operation.c
+++ src/roman_numeral_operation.c
@@ -1,5 +1,11 @@
 #include "headers/roman_numeral_operation.h"
 
 bool add_two_roman_numerals(char ** result, char * lhs, char * rhs){
-  return false;
+  unsigned int lhs_arabic_value = convert_to_arabic_value_from_roman_numeral(lhs);
+  unsigned int rhs_arabic_value = convert_to_arabic_value_from_roman_numeral(rhs);
+
+  unsigned int sum_arabic_value = lhs_arabic_value + rhs_arabic_value;
+
+  convert_to_roman_numeral_from_arabic_value(result, sum_arabic_value);
+  return true;
 }
