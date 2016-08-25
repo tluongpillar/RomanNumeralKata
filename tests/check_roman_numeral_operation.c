00f239d328975c520269e0eb1280a032bfc98f90
Thinh Luong <tluong@pillartechnology.com>
2016-08-24 21:48:30 -0500

    Added and passed test add_two_roman_numerals given I and III equals IV.

--- tests/check_roman_numeral_operation.c
+++ tests/check_roman_numeral_operation.c
@@ -1,5 +1,22 @@
 #include "headers/check_roman_numeral_operation.h"
 
+START_TEST(test_add_two_roman_numerals__given_I_add_III__returns_IV)
+{
+  char * result = (char*)malloc(strlen(MAX_ROMAN_NUMERAL) * sizeof(char));
+  char ** result_pointer;
+  result_pointer = &result;
+
+  add_two_roman_numerals(result_pointer, "I", "III");
+
+  ck_assert_str_eq("IV", *result_pointer);
+
+  free(result);
+  result = NULL;
+  result_pointer = NULL;
+
+}
+END_TEST
+
 START_TEST(test_add_two_roman_numerals__given_I_add_II__returns_III)
 {
   char * result = (char*)malloc(strlen(MAX_ROMAN_NUMERAL) * sizeof(char));
@@ -42,6 +59,7 @@ Suite * roman_numeral_operation_suite()
   suite = suite_create("RomanNumeralOperation");
   tcase_core = tcase_create("Core");
 
+  tcase_add_test(tcase_core, test_add_two_roman_numerals__given_I_add_III__returns_IV);
   tcase_add_test(tcase_core, test_add_two_roman_numerals__given_I_add_II__returns_III);
   tcase_add_test(tcase_core, test_add_two_roman_numerals__given_I_add_I__returns_II);
 
