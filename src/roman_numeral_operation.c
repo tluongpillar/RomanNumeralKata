690f55a98a5eeff52b2b3a0faf7fbf9d59f2537a
Thinh Luong <tluong@pillartechnology.com>
2016-08-26 11:54:47 -0500

    Passed test I-II should return false and II-I should return true.

--- src/roman_numeral_operation.c
+++ src/roman_numeral_operation.c
@@ -14,7 +14,6 @@ bool add_two_roman_numerals(char ** result, char * lhs, char * rhs){
   {
     return false;
   }
-
 }
 
 bool subtract_two_roman_numerals(char ** result, char * lhs, char * rhs)
@@ -22,8 +21,13 @@ bool subtract_two_roman_numerals(char ** result, char * lhs, char * rhs)
   unsigned int lhs_arabic_value = convert_to_arabic_value_from_roman_numeral(lhs);
   unsigned int rhs_arabic_value = convert_to_arabic_value_from_roman_numeral(rhs);
 
-  unsigned int result_arabic_value = lhs_arabic_value - rhs_arabic_value;
-
-  convert_to_roman_numeral_from_arabic_value(result, result_arabic_value);
-  return false;
+  if (lhs_arabic_value >= rhs_arabic_value)
+  {
+    unsigned int result_arabic_value = lhs_arabic_value - rhs_arabic_value;
+    convert_to_roman_numeral_from_arabic_value(result, result_arabic_value);
+    return true;
+  } else
+  {
+    return false;
+  }
 }
