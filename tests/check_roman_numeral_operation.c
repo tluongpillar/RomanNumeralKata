14ebf912d6aa6dc360929665b4b2dd4cc1f55e8f
Thinh Luong <tluong@pillartechnology.com>
2016-08-25 21:39:36 -0500

    Updated test to account for all combination of inputs to add function and updated tcase timeout limit to one hour.

--- tests/check_roman_numeral_operation.c
+++ tests/check_roman_numeral_operation.c
@@ -1,6 +1,6 @@
 #include "headers/check_roman_numeral_operation.h"
 
-START_TEST(test_add_two_roman_numerals__given_I_add_I_to_MMMCMXCVIII__returns_II_to_MMMCMXCIX)
+START_TEST(test_add_two_roman_numerals__given_all_input_combination__returns_correct_sum)
 {
   char * expected_result = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
   char ** expected_result_pointer;
@@ -10,33 +10,46 @@ START_TEST(test_add_two_roman_numerals__given_I_add_I_to_MMMCMXCVIII__returns_II
   char ** actual_result_pointer;
   actual_result_pointer = &actual_result;
 
-  unsigned int expected_result_arabic_value = 0;
-
-  char * lhs_roman_numeral = "I";
-  unsigned int lhs_arabic_value = convert_to_arabic_value_from_roman_numeral(lhs_roman_numeral);
+  char * lhs_roman_numeral = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
+  char ** lhs_roman_numeral_pointer;
+  lhs_roman_numeral_pointer = &lhs_roman_numeral;
 
   char * rhs_roman_numeral = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
   char ** rhs_roman_numeral_pointer;
   rhs_roman_numeral_pointer = &rhs_roman_numeral;
 
+  unsigned int expected_result_arabic_value;
   unsigned int rhs_arabic_value;
-  unsigned int rhs_arabic_value_upper_limit = MAX_ROMAN_NUMERAL_ARABIC_VALUE - lhs_arabic_value;
-  for (rhs_arabic_value = 1; rhs_arabic_value <= rhs_arabic_value_upper_limit; ++rhs_arabic_value)
+  unsigned int rhs_arabic_value_upper_limit;
+  unsigned int lhs_arabic_value;
+  unsigned int lhs_arabic_value_upper_limit = MAX_ROMAN_NUMERAL_ARABIC_VALUE - 1;
+  for (lhs_arabic_value = 1; lhs_arabic_value <= lhs_arabic_value_upper_limit; ++lhs_arabic_value)
   {
-    expected_result_arabic_value = lhs_arabic_value + rhs_arabic_value;
+    rhs_arabic_value_upper_limit = MAX_ROMAN_NUMERAL_ARABIC_VALUE - lhs_arabic_value;
+    for (rhs_arabic_value = 1; rhs_arabic_value <= rhs_arabic_value_upper_limit; ++rhs_arabic_value)
+    {
+      expected_result_arabic_value = lhs_arabic_value + rhs_arabic_value;
 
-    convert_to_roman_numeral_from_arabic_value(expected_result_pointer, expected_result_arabic_value);
+      convert_to_roman_numeral_from_arabic_value(expected_result_pointer, expected_result_arabic_value);
 
-    convert_to_roman_numeral_from_arabic_value(rhs_roman_numeral_pointer, rhs_arabic_value);
+      convert_to_roman_numeral_from_arabic_value(lhs_roman_numeral_pointer, lhs_arabic_value);
+      convert_to_roman_numeral_from_arabic_value(rhs_roman_numeral_pointer, rhs_arabic_value);
 
-    add_two_roman_numerals(actual_result_pointer, lhs_roman_numeral, rhs_roman_numeral);
+      add_two_roman_numerals(actual_result_pointer, lhs_roman_numeral, rhs_roman_numeral);
 
-    ck_assert_str_eq(expected_result, actual_result);
+      ck_assert_str_eq(expected_result, actual_result);
 
-    memset(rhs_roman_numeral, 0, strlen(rhs_roman_numeral));
-    memset(expected_result, 0, strlen(expected_result));
-    memset(actual_result, 0, strlen(actual_result));
+      printf("%s + %s = %s\n", lhs_roman_numeral, rhs_roman_numeral, actual_result);
+      memset(lhs_roman_numeral, 0, strlen(lhs_roman_numeral));
+      memset(rhs_roman_numeral, 0, strlen(rhs_roman_numeral));
+      memset(expected_result, 0, strlen(expected_result));
+      memset(actual_result, 0, strlen(actual_result));
+    }
   }
+
+  free(lhs_roman_numeral);
+  lhs_roman_numeral = NULL;
+  lhs_roman_numeral_pointer = NULL;
 
   free(rhs_roman_numeral);
   rhs_roman_numeral = NULL;
@@ -49,7 +62,6 @@ START_TEST(test_add_two_roman_numerals__given_I_add_I_to_MMMCMXCVIII__returns_II
   free(expected_result);
   expected_result = NULL;
   expected_result_pointer = NULL;
-
 }
 END_TEST
 
@@ -61,7 +73,8 @@ Suite * roman_numeral_operation_suite()
   suite = suite_create("RomanNumeralOperation");
   tcase_core = tcase_create("Core");
 
-  tcase_add_test(tcase_core, test_add_two_roman_numerals__given_I_add_I_to_MMMCMXCVIII__returns_II_to_MMMCMXCIX);
+  tcase_set_timeout(tcase_core, 3600);
+  tcase_add_test(tcase_core, test_add_two_roman_numerals__given_all_input_combination__returns_correct_sum);
 
   suite_add_tcase(suite, tcase_core);
 
