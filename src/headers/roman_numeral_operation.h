daa9db4f08e1bb4a55ac7c82a20f3100c1c554a6
Thinh Luong <tluong@pillartechnology.com>
2016-08-25 21:57:11 -0500

    Passed test for subtraction function with II - I return I.

--- src/headers/roman_numeral_operation.h
+++ src/headers/roman_numeral_operation.h
@@ -4,3 +4,4 @@
 #include "roman_numeral_conversion.h"
 
 bool add_two_roman_numerals(char ** result, char * lhs, char * rhs);
+bool subtract_two_roman_numerals(char ** result, char * lhs, char * rhs);
--- src/roman_numeral_operation.c
+++ src/roman_numeral_operation.c
@@ -9,3 +9,14 @@ bool add_two_roman_numerals(char ** result, char * lhs, char * rhs){
   convert_to_roman_numeral_from_arabic_value(result, sum_arabic_value);
   return true;
 }
+
+bool subtract_two_roman_numerals(char ** result, char * lhs, char * rhs)
+{
+  unsigned int lhs_arabic_value = convert_to_arabic_value_from_roman_numeral(lhs);
+  unsigned int rhs_arabic_value = convert_to_arabic_value_from_roman_numeral(rhs);
+
+  unsigned int result_arabic_value = lhs_arabic_value - rhs_arabic_value;
+
+  convert_to_roman_numeral_from_arabic_value(result, result_arabic_value);
+  return false;
+}
