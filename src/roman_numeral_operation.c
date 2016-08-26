b855476569c6c39352765999437c381e0996c697
Thinh Luong <tluong@pillartechnology.com>
2016-08-25 22:21:38 -0500

    Passed test for adding two roman numerals and going over the maximum roman numeral 3999 (MMMCMXCIX).

--- src/roman_numeral_operation.c
+++ src/roman_numeral_operation.c
@@ -6,8 +6,15 @@ bool add_two_roman_numerals(char ** result, char * lhs, char * rhs){
 
   unsigned int sum_arabic_value = lhs_arabic_value + rhs_arabic_value;
 
-  convert_to_roman_numeral_from_arabic_value(result, sum_arabic_value);
-  return true;
+  if (sum_arabic_value <= MAX_ROMAN_NUMERAL_ARABIC_VALUE)
+  {
+    convert_to_roman_numeral_from_arabic_value(result, sum_arabic_value);
+    return true;
+  } else
+  {
+    return false;
+  }
+
 }
 
 bool subtract_two_roman_numerals(char ** result, char * lhs, char * rhs)
