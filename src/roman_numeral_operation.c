6486ffb71cc7e31a7b9ced8f6c7d7a833b87864e
Thinh Luong <tluong@pillartechnology.com>
2016-08-27 15:44:11 -0500

    Passed test is_valid_roman_numeral given invalid out of order roman numeral returns false.

--- src/roman_numeral_operation.c
+++ src/roman_numeral_operation.c
@@ -43,5 +43,21 @@ bool is_valid_roman_numeral(char * roman_numeral)
     }
   }
 
-  return true;
+  unsigned int roman_numeral_arabic_value = convert_to_arabic_value_from_roman_numeral(roman_numeral);
+  char * expected_roman_numeral = calloc(strlen(MAX_ROMAN_NUMERAL) + 1, sizeof(char));
+  char ** expected_roman_numeral_pointer;
+  expected_roman_numeral_pointer = &expected_roman_numeral;
+
+  convert_to_roman_numeral_from_arabic_value(expected_roman_numeral_pointer, roman_numeral_arabic_value);
+
+  if (strcmp(expected_roman_numeral, roman_numeral) == 0)
+  {
+    return true;
+  }
+
+  free(expected_roman_numeral);
+  expected_roman_numeral = NULL;
+  expected_roman_numeral_pointer = NULL;
+
+  return false;
 }
