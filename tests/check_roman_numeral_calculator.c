ca3e366189e0cc2d8bf37335aca1817afd7a4a4c
Thinh Luong <tluong@pillartechnology.com>
2016-08-28 15:26:40 -0500

    Added test perform_operation_with_roman_numerals given I + II should return III with success.

--- tests/check_roman_numeral_calculator.c
+++ tests/check_roman_numeral_calculator.c
@@ -1,8 +1,19 @@
 #include "headers/check_roman_numeral_calculator.h"
 
-START_TEST(test_first)
+START_TEST(test_perform_operation_with_roman_numerals__given_I_II_add__returns_III_with_success)
 {
-  ck_assert(false);
+  char * actual_result = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
+  char ** actual_result_pointer;
+  actual_result_pointer = &actual_result;
+
+  bool did_perform_successfully = perform_operation_with_roman_numerals(actual_result_pointer, "I", "II", "+");
+
+  ck_assert(true == did_perform_successfully);
+  ck_assert_str_eq("III", actual_result);
+
+  free(actual_result);
+  actual_result = NULL;
+  actual_result_pointer = NULL;
 }
 END_TEST
 
@@ -14,7 +25,7 @@ Suite * roman_numeral_calculator_suite()
   suite = suite_create("RomanNumeralCalculator");
   tcase_core = tcase_create("Core");
 
-  tcase_add_test(tcase_core, test_first);
+  tcase_add_test(tcase_core, test_perform_operation_with_roman_numerals__given_I_II_add__returns_III_with_success);
   suite_add_tcase(suite, tcase_core);
 
   return suite;
