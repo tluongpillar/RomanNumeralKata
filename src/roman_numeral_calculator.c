0534f25dd28d41d606699a61d17e1b7673543ee1
Thinh Luong <tluong@pillartechnology.com>
2016-08-27 20:23:42 -0500

    Updated main program to capture user input for first and second roman numerals.

--- src/roman_numeral_calculator.c
+++ src/roman_numeral_calculator.c
@@ -7,10 +7,23 @@ int main()
   bool perform_another_calculation = true;
   char * user_input = calloc(20, sizeof(char));
 
+  char * first_roman_numeral = calloc(20, sizeof(char));
+  char * second_roman_numeral = calloc(20, sizeof(char));
+
   while (perform_another_calculation)
   {
-    printf("Would you like to perform another calculation? (Y/N): ");
+    printf("Please enter the first roman_numeral: ");
     scanf("%s", user_input);
+    strcpy(first_roman_numeral, user_input);
+    memset(user_input, 0, strlen(user_input));
+
+    printf("Please enter the second roman_numeral: ");
+    scanf("%s", user_input);
+    strcpy(second_roman_numeral, user_input);
+    memset(user_input, 0, strlen(user_input));
+
+    printf("Would you like to perform another calculation? (Y/N): ");
+    scanf("\n%s", user_input);
     if (strcmp("N", user_input) == 0)
     {
       perform_another_calculation = false;
@@ -20,6 +33,6 @@ int main()
 
   free(user_input);
   user_input = NULL;
-  
+
   return 0;
 }
