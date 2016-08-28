a3fd2c92736ee6b721330e456eac68aa4807511e
Thinh Luong <tluong@pillartechnology.com>
2016-08-28 17:30:50 -0500

    Added and passed test perform_operation_with_roman_numerals with addition of two roman numeral resulting in sum greater than max allowed roman numeral returns empty string and failure. Fixed missing include in suite runner.

--- tests/check_roman_numeral_calculator.c
+++ tests/check_roman_numeral_calculator.c
@@ -17,6 +17,23 @@ START_TEST(test_perform_operation_with_roman_numerals__given_I_II_add__returns_I
 }
 END_TEST
 
+START_TEST(test_perform_operation_with_roman_numerals__given_MAX_ROMAN_NUMERAL_I_add__returns_empty_string_with_failure)
+{
+  char * actual_result = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
+  char ** actual_result_pointer;
+  actual_result_pointer = &actual_result;
+
+  bool did_perform_successfully = perform_operation_with_roman_numerals(actual_result_pointer, MAX_ROMAN_NUMERAL, "I", "+");
+
+  ck_assert(false == did_perform_successfully);
+  ck_assert_str_eq("\0", actual_result);
+
+  free(actual_result);
+  actual_result = NULL;
+  actual_result_pointer = NULL;
+}
+END_TEST
+
 Suite * roman_numeral_calculator_suite()
 {
   Suite *suite;
@@ -25,6 +42,7 @@ Suite * roman_numeral_calculator_suite()
   suite = suite_create("RomanNumeralCalculator");
   tcase_core = tcase_create("Core");
 
+  tcase_add_test(tcase_core, test_perform_operation_with_roman_numerals__given_MAX_ROMAN_NUMERAL_I_add__returns_empty_string_with_failure);
   tcase_add_test(tcase_core, test_perform_operation_with_roman_numerals__given_I_II_add__returns_III_with_success);
   suite_add_tcase(suite, tcase_core);
 
