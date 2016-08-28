7fcd5da96ec666a1f0ac7edf67e755cd0fef361f
Thinh Luong <tluong@pillartechnology.com>
2016-08-28 13:05:05 -0500

    Refactored to using helper function for requesting second roman numeral and added helper function for requesting operation.

--- src/roman_numeral_calculator.c
+++ src/roman_numeral_calculator.c
@@ -1,14 +1,13 @@
 #include "headers/roman_numeral_calculator.h"
 
 static void request_user_for_roman_numeral_input(char ** roman_numeral);
+static void request_user_for_operation_input(char ** operation);
 
 int main()
 {
   printf("Welcome to the Roman Numeral Calculator!\n");
 
   bool perform_another_calculation = true;
-  bool verifying_operation = true;
-  bool verifying_second_roman_numeral = true;
 
   char * user_input = calloc(20, sizeof(char));
   char * first_roman_numeral = calloc(20, sizeof(char));
@@ -16,46 +15,23 @@ int main()
   first_roman_numeral_pointer = &first_roman_numeral;
 
   char * second_roman_numeral = calloc(20, sizeof(char));
+  char ** second_roman_numeral_pointer;
+  second_roman_numeral_pointer = &second_roman_numeral;
+
   char * operation = calloc(20, sizeof(char));
+  char ** operation_pointer;
+  operation_pointer = &operation;
 
   while (perform_another_calculation)
   {
     printf("Please enter the first roman numeral: ");
     request_user_for_roman_numeral_input(first_roman_numeral_pointer);
 
-    while (verifying_second_roman_numeral)
-    {
-      printf("Please enter the second roman_numeral: ");
-      scanf("%s", user_input);
+    printf("Please enter the second roman numeral: ");
+    request_user_for_roman_numeral_input(second_roman_numeral_pointer);
 
-      if (is_valid_roman_numeral(user_input))
-      {
-        strcpy(second_roman_numeral, user_input);
-        verifying_second_roman_numeral = false;
-      } else
-      {
-        printf("You have entered an invalid roman numeral.\n");
-      }
-
-      memset(user_input, 0, strlen(user_input));
-    }
-
-    while (verifying_operation)
-    {
-      printf("Please enter the desired operation (+/-): ");
-      scanf("%s", user_input);
-      strcpy(operation, user_input);
-
-      memset(user_input, 0, strlen(user_input));
-
-      if (strcmp("+", operation) == 0 || strcmp("-", operation) == 0)
-      {
-        verifying_operation = false;
-      } else
-      {
-        memset(operation, 0, strlen(operation));
-      }
-    }
+    printf("Please enter the desired operation (+/-): ");
+    request_user_for_operation_input(operation_pointer);
 
     printf("%s %s %s = TBD\n", first_roman_numeral, operation, second_roman_numeral);
 
@@ -65,9 +41,6 @@ int main()
     {
       perform_another_calculation = false;
     }
-
-    verifying_operation = true;
-    verifying_second_roman_numeral = true;
 
     memset(user_input, 0, strlen(user_input));
     memset(first_roman_numeral, 0, strlen(first_roman_numeral));
@@ -82,8 +55,10 @@ int main()
   first_roman_numeral_pointer = NULL;
   free(second_roman_numeral);
   second_roman_numeral = NULL;
+  second_roman_numeral_pointer = NULL;
   free(operation);
   operation = NULL;
+  operation_pointer = NULL;
 
   return 0;
 }
@@ -104,6 +79,24 @@ static void request_user_for_roman_numeral_input(char ** roman_numeral)
     {
       printf("Invalid roman numeral. Please try again: ");
       memset(*roman_numeral, 0, strlen(*roman_numeral));
+    }
+  }
+}
+
+static void request_user_for_operation_input(char ** operation)
+{
+  bool verifying_operation = true;
+  while (verifying_operation)
+  {
+    scanf("%s", *operation);
+
+    if (strcmp("+", *operation) == 0 || strcmp("-", *operation) == 0)
+    {
+      verifying_operation = false;
+    } else
+    {
+      printf("Invalid operation. Please try again (+/-): ");
+      memset(*operation, 0, strlen(*operation));
     }
   }
 }
