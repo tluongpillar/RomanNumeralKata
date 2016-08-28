aab52757cede5b5a800eb82bcb148918675c2b0e
Thinh Luong <tluong@pillartechnology.com>
2016-08-28 17:35:10 -0500

    Added test for II - I  should return I with success.

--- tests/check_roman_numeral_calculator.c
+++ tests/check_roman_numeral_calculator.c
@@ -34,6 +34,23 @@ START_TEST(test_perform_operation_with_roman_numerals__given_MAX_ROMAN_NUMERAL_I
 }
 END_TEST
 
+START_TEST(test_perform_operation_with_roman_numerals__given_II_I_subtract__returns_I_with_success)
+{
+  char * actual_result = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
+  char ** actual_result_pointer;
+  actual_result_pointer = &actual_result;
+
+  bool did_perform_successfully = perform_operation_with_roman_numerals(actual_result_pointer, "II", "I", "-");
+
+  ck_assert(true == did_perform_successfully);
+  ck_assert_str_eq("I", actual_result);
+
+  free(actual_result);
+  actual_result = NULL;
+  actual_result_pointer = NULL;
+}
+END_TEST
+
 Suite * roman_numeral_calculator_suite()
 {
   Suite *suite;
@@ -42,6 +59,7 @@ Suite * roman_numeral_calculator_suite()
   suite = suite_create("RomanNumeralCalculator");
   tcase_core = tcase_create("Core");
 
+  tcase_add_test(tcase_core, test_perform_operation_with_roman_numerals__given_II_I_subtract__returns_I_with_success);
   tcase_add_test(tcase_core, test_perform_operation_with_roman_numerals__given_MAX_ROMAN_NUMERAL_I_add__returns_empty_string_with_failure);
   tcase_add_test(tcase_core, test_perform_operation_with_roman_numerals__given_I_II_add__returns_III_with_success);
   suite_add_tcase(suite, tcase_core);
