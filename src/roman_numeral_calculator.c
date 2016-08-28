f3b46ff1e4bd76c5b586ec9c8dfbe6f547a9640f
Thinh Luong <tluong@pillartechnology.com>
2016-08-27 19:36:44 -0500

    Added main loop for user to perform another operation.

--- src/roman_numeral_calculator.c
+++ src/roman_numeral_calculator.c
@@ -3,5 +3,23 @@
 int main()
 {
   printf("Welcome to the Roman Numeral Calculator!\n");
+
+  bool perform_another_calculation = true;
+  char * user_input = calloc(20, sizeof(char));
+
+  while (perform_another_calculation)
+  {
+    printf("Would you like to perform another calculation? (Y/N): ");
+    scanf("%s", user_input);
+    if (strcmp("N", user_input) == 0)
+    {
+      perform_another_calculation = false;
+    }
+    memset(user_input, 0, strlen(user_input));
+  }
+
+  free(user_input);
+  user_input = NULL;
+  
   return 0;
 }
