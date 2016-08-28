935be4006af508a38c79629b002a2d68d60ab260
Thinh Luong <tluong@pillartechnology.com>
2016-08-28 08:50:47 -0500

    Updated main program loop to ask user for a valid +/- operation.

--- src/roman_numeral_calculator.c
+++ src/roman_numeral_calculator.c
@@ -5,8 +5,9 @@ int main()
   printf("Welcome to the Roman Numeral Calculator!\n");
 
   bool perform_another_calculation = true;
-  char * user_input = calloc(20, sizeof(char));
+  bool verifying_operation = true;
 
+  char * user_input = calloc(20, sizeof(char));
   char * first_roman_numeral = calloc(20, sizeof(char));
   char * second_roman_numeral = calloc(20, sizeof(char));
   char * operation = calloc(20, sizeof(char));
@@ -23,10 +24,22 @@ int main()
     strcpy(second_roman_numeral, user_input);
     memset(user_input, 0, strlen(user_input));
 
-    printf("Please enter the desired operation (+/-): ");
-    scanf("%s", user_input);
-    strcpy(operation, user_input);
-    memset(user_input, 0, strlen(user_input));
+    while (verifying_operation)
+    {
+      printf("Please enter the desired operation (+/-): ");
+      scanf("%s", user_input);
+      strcpy(operation, user_input);
+
+      memset(user_input, 0, strlen(user_input));
+
+      if (strcmp("+", operation) == 0 || strcmp("-", operation) == 0)
+      {
+        verifying_operation = false;
+      } else
+      {
+        memset(operation, 0, strlen(operation));
+      }
+    }
 
     printf("%s %s %s = TBD\n", first_roman_numeral, operation, second_roman_numeral);
 
@@ -36,11 +49,13 @@ int main()
     {
       perform_another_calculation = false;
     }
+
+    verifying_operation = true;
+
     memset(user_input, 0, strlen(user_input));
     memset(first_roman_numeral, 0, strlen(first_roman_numeral));
     memset(second_roman_numeral, 0, strlen(second_roman_numeral));
     memset(operation, 0, strlen(operation));
-
   }
 
   free(user_input);
