5dae054fc80e750dacf1218b475d67cb44b94d83
Thinh Luong <tluong@pillartechnology.com>
2016-08-28 17:40:21 -0500

    Passed test for II - I  should return I with success.

--- src/roman_numeral_calculator.c
+++ src/roman_numeral_calculator.c
@@ -9,6 +9,9 @@ bool perform_operation_with_roman_numerals(char ** result, char * lhs_roman_nume
     case '+':
     operation_return_status = add_two_roman_numerals(result, lhs_roman_numeral, rhs_roman_numeral);
     break;
+    case '-':
+    operation_return_status = subtract_two_roman_numerals(result, lhs_roman_numeral, rhs_roman_numeral);
+    break;
     default:
     break;
   }
