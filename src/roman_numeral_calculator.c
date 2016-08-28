6de94e964b7bc54a0dc28048a2b9ba5feefc251c
Thinh Luong <tluong@pillartechnology.com>
2016-08-27 20:37:30 -0500

    Updated main program to include stdlib, capture user's operation input, and free up memory at the end of the program.

--- src/roman_numeral_calculator.c
+++ src/roman_numeral_calculator.c
@@ -9,6 +9,7 @@ int main()
 
   char * first_roman_numeral = calloc(20, sizeof(char));
   char * second_roman_numeral = calloc(20, sizeof(char));
+  char * operation = calloc(20, sizeof(char));
 
   while (perform_another_calculation)
   {
@@ -22,6 +23,13 @@ int main()
     strcpy(second_roman_numeral, user_input);
     memset(user_input, 0, strlen(user_input));
 
+    printf("Please enter the desired operation (+/-): ");
+    scanf("%s", user_input);
+    strcpy(operation, user_input);
+    memset(user_input, 0, strlen(user_input));
+
+    printf("%s %s %s = TBD\n", first_roman_numeral, operation, second_roman_numeral);
+
     printf("Would you like to perform another calculation? (Y/N): ");
     scanf("\n%s", user_input);
     if (strcmp("N", user_input) == 0)
@@ -29,10 +37,20 @@ int main()
       perform_another_calculation = false;
     }
     memset(user_input, 0, strlen(user_input));
+    memset(first_roman_numeral, 0, strlen(first_roman_numeral));
+    memset(second_roman_numeral, 0, strlen(second_roman_numeral));
+    memset(operation, 0, strlen(operation));
+
   }
 
   free(user_input);
   user_input = NULL;
+  free(first_roman_numeral);
+  first_roman_numeral = NULL;
+  free(second_roman_numeral);
+  second_roman_numeral = NULL;
+  free(operation);
+  operation = NULL;
 
   return 0;
 }
