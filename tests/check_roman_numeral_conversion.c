f755e91bed1e5862286638468ab4c30624e40e20
Thinh Luong <tluong@pillartechnology.com>
2016-08-28 19:27:20 -0500

    Refactored test to use for loop from 1-900.

--- tests/check_roman_numeral_conversion.c
+++ tests/check_roman_numeral_conversion.c
@@ -122,43 +122,55 @@ END_TEST
 
 START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_I_through_IX__returns_1_through_9)
 {
-  ck_assert_uint_eq(1, convert_to_arabic_value_from_roman_numeral("I"));
-  ck_assert_uint_eq(2, convert_to_arabic_value_from_roman_numeral("II"));
-  ck_assert_uint_eq(3, convert_to_arabic_value_from_roman_numeral("III"));
-  ck_assert_uint_eq(4, convert_to_arabic_value_from_roman_numeral("IV"));
-  ck_assert_uint_eq(5, convert_to_arabic_value_from_roman_numeral("V"));
-  ck_assert_uint_eq(6, convert_to_arabic_value_from_roman_numeral("VI"));
-  ck_assert_uint_eq(7, convert_to_arabic_value_from_roman_numeral("VII"));
-  ck_assert_uint_eq(8, convert_to_arabic_value_from_roman_numeral("VIII"));
-  ck_assert_uint_eq(9, convert_to_arabic_value_from_roman_numeral("IX"));
+  char * input_roman_numeral[] = {
+    "\0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"
+  };
+
+  unsigned int expected_arabic_value[] = {
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+  };
+
+  int index;
+  for (index = 0; index <= 9; ++index)
+  {
+    ck_assert_uint_eq(expected_arabic_value[index], convert_to_arabic_value_from_roman_numeral(input_roman_numeral[index]));
+  }
 }
 END_TEST
 
 START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_X_through_XC_by_X__returns_10_through_90_by_10)
 {
-  ck_assert_uint_eq(10, convert_to_arabic_value_from_roman_numeral("X"));
-  ck_assert_uint_eq(20, convert_to_arabic_value_from_roman_numeral("XX"));
-  ck_assert_uint_eq(30, convert_to_arabic_value_from_roman_numeral("XXX"));
-  ck_assert_uint_eq(40, convert_to_arabic_value_from_roman_numeral("XL"));
-  ck_assert_uint_eq(50, convert_to_arabic_value_from_roman_numeral("L"));
-  ck_assert_uint_eq(60, convert_to_arabic_value_from_roman_numeral("LX"));
-  ck_assert_uint_eq(70, convert_to_arabic_value_from_roman_numeral("LXX"));
-  ck_assert_uint_eq(80, convert_to_arabic_value_from_roman_numeral("LXXX"));
-  ck_assert_uint_eq(90, convert_to_arabic_value_from_roman_numeral("XC"));
+  char * input_roman_numeral[] = {
+    "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"
+  };
+
+  unsigned int expected_arabic_value[] = {
+    10, 20, 30, 40, 50, 60, 70, 80, 90
+  };
+
+  int index;
+  for (index = 0; index <= 8; ++index)
+  {
+    ck_assert_uint_eq(expected_arabic_value[index], convert_to_arabic_value_from_roman_numeral(input_roman_numeral[index]));
+  }
 }
 END_TEST
 
 START_TEST(test_convert_to_arabic_value_from_roman_numeral__given_C_through_CM_by_C__returns_100_through_900_by_100)
 {
-  ck_assert_uint_eq(100, convert_to_arabic_value_from_roman_numeral("C"));
-  ck_assert_uint_eq(200, convert_to_arabic_value_from_roman_numeral("CC"));
-  ck_assert_uint_eq(300, convert_to_arabic_value_from_roman_numeral("CCC"));
-  ck_assert_uint_eq(400, convert_to_arabic_value_from_roman_numeral("CD"));
-  ck_assert_uint_eq(500, convert_to_arabic_value_from_roman_numeral("D"));
-  ck_assert_uint_eq(600, convert_to_arabic_value_from_roman_numeral("DC"));
-  ck_assert_uint_eq(700, convert_to_arabic_value_from_roman_numeral("DCC"));
-  ck_assert_uint_eq(800, convert_to_arabic_value_from_roman_numeral("DCCC"));
-  ck_assert_uint_eq(900, convert_to_arabic_value_from_roman_numeral("CM"));
+  char * input_roman_numeral[] = {
+    "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"
+  };
+
+  unsigned int expected_arabic_value[] = {
+    100, 200, 300, 400, 500, 600, 700, 800, 900
+  };
+
+  int index;
+  for (index = 0; index <= 8; ++index)
+  {
+    ck_assert_uint_eq(expected_arabic_value[index], convert_to_arabic_value_from_roman_numeral(input_roman_numeral[index]));
+  }
 }
 END_TEST
 
