fce135aaf1d0ed00774d5fea74de419cd837cd3b
Thinh Luong <tluong@pillartechnology.com>
2016-09-04 18:37:23 -0500

    Refactored subtract_two_roman_numerals test given all valid input combination to use loop test and checked test fixtures.

--- tests/check_roman_numeral_operation.c
+++ tests/check_roman_numeral_operation.c
@@ -125,51 +125,26 @@ END_TEST
 
 START_TEST(test_subtract_two_roman_numerals__given_all_valid_combination_inputs___returns_correct_result_with_success)
 {
-  char * lhs_roman_numeral = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
-  char * rhs_roman_numeral = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
-  char * expected_result = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
-  char * actual_result = calloc((strlen(MAX_ROMAN_NUMERAL) + 1), sizeof(char));
-
-  bool did_subtract_successfully = false;
+  const unsigned int lhs_arabic_value = _i;
   unsigned int expected_result_arabic_value;
-  unsigned int lhs_arabic_value;
   unsigned int rhs_arabic_value;
 
-  for (lhs_arabic_value = MAX_ROMAN_NUMERAL_ARABIC_VALUE; lhs_arabic_value >= 0 && lhs_arabic_value <= MAX_ROMAN_NUMERAL_ARABIC_VALUE; --lhs_arabic_value)
+  for (rhs_arabic_value = lhs_arabic_value; rhs_arabic_value >= 0 && rhs_arabic_value <= lhs_arabic_value; --rhs_arabic_value)
   {
-    for (rhs_arabic_value = lhs_arabic_value; rhs_arabic_value >= 0 && rhs_arabic_value <= lhs_arabic_value; --rhs_arabic_value)
-    {
-      expected_result_arabic_value = lhs_arabic_value - rhs_arabic_value;
-      convert_to_roman_numeral_from_arabic_value(expected_result, expected_result_arabic_value);
+    expected_result_arabic_value = lhs_arabic_value - rhs_arabic_value;
+    convert_to_roman_numeral_from_arabic_value(expected_result, expected_result_arabic_value);
+    convert_to_roman_numeral_from_arabic_value(lhs_roman_numeral, lhs_arabic_value);
+    convert_to_roman_numeral_from_arabic_value(rhs_roman_numeral, rhs_arabic_value);
 
-      convert_to_roman_numeral_from_arabic_value(lhs_roman_numeral, lhs_arabic_value);
-      convert_to_roman_numeral_from_arabic_value(rhs_roman_numeral, rhs_arabic_value);
+    subtract_two_roman_numerals(actual_result, lhs_roman_numeral, rhs_roman_numeral);
 
-      did_subtract_successfully = subtract_two_roman_numerals(actual_result, lhs_roman_numeral, rhs_roman_numeral);
+    ck_assert_str_eq(expected_result, actual_result);
 
-      ck_assert(true == did_subtract_successfully);
-      ck_assert_str_eq(expected_result, actual_result);
-
-      did_subtract_successfully = false;
-
-      memset(lhs_roman_numeral, 0, strlen(lhs_roman_numeral));
-      memset(rhs_roman_numeral, 0, strlen(rhs_roman_numeral));
-      memset(expected_result, 0, strlen(expected_result));
-      memset(actual_result, 0, strlen(actual_result));
-    }
+    memset(lhs_roman_numeral, 0, strlen(lhs_roman_numeral));
+    memset(rhs_roman_numeral, 0, strlen(rhs_roman_numeral));
+    memset(expected_result, 0, strlen(expected_result));
+    memset(actual_result, 0, strlen(actual_result));
   }
-
-  free(actual_result);
-  actual_result = NULL;
-
-  free(expected_result);
-  expected_result = NULL;
-
-  free(lhs_roman_numeral);
-  lhs_roman_numeral = NULL;
-
-  free(rhs_roman_numeral);
-  rhs_roman_numeral = NULL;
 }
 END_TEST
 
@@ -220,7 +195,7 @@ Suite * roman_numeral_operation_suite()
   tcase_add_test(tcase_core, test_subtract_two_roman_numerals__given_I_minus_II__returns_empty_string);
   tcase_add_test(tcase_core, test_subtract_two_roman_numerals__given_I_minus_II__returns_false);
   tcase_add_test(tcase_core, test_subtract_two_roman_numerals__given_II_minus_I__returns_true);
-  tcase_add_test(tcase_core, test_subtract_two_roman_numerals__given_all_valid_combination_inputs___returns_correct_result_with_success);
+  tcase_add_loop_test(tcase_core, test_subtract_two_roman_numerals__given_all_valid_combination_inputs___returns_correct_result_with_success, start_index, MAX_ROMAN_NUMERAL_ARABIC_VALUE + 1);
 
   suite_add_tcase(suite, tcase_core);
 
