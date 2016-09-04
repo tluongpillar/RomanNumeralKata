b41c0e28790dfeab5504476a4987cc2ebc73127f
Thinh Luong <tluong@pillartechnology.com>
2016-09-04 11:37:57 -0500

    Refactored implemenation and passed tests for subtract_two_roman_numerals to use char* instead of char**

--- include/roman_numeral_operation.h
+++ include/roman_numeral_operation.h
@@ -4,5 +4,5 @@
 #include "roman_numeral_conversion.h"
 
 bool add_two_roman_numerals(char * result, const char * lhs, const char * rhs);
-bool subtract_two_roman_numerals(char ** result, const char * lhs, const char * rhs);
+bool subtract_two_roman_numerals(char * result, const char * lhs, const char * rhs);
 bool is_valid_roman_numeral(const char * roman_numeral);
--- src/roman_numeral_operation.c
+++ src/roman_numeral_operation.c
@@ -17,7 +17,7 @@ bool add_two_roman_numerals(char * result, const char * lhs, const char * rhs){
   }
 }
 
-bool subtract_two_roman_numerals(char ** result, const char * lhs, const char * rhs)
+bool subtract_two_roman_numerals(char * result, const char * lhs, const char * rhs)
 {
   unsigned int lhs_arabic_value = convert_to_arabic_value_from_roman_numeral(lhs);
   unsigned int rhs_arabic_value = convert_to_arabic_value_from_roman_numeral(rhs);
@@ -25,7 +25,7 @@ bool subtract_two_roman_numerals(char ** result, const char * lhs, const char * 
   if (lhs_arabic_value >= rhs_arabic_value)
   {
     unsigned int result_arabic_value = lhs_arabic_value - rhs_arabic_value;
-    convert_to_roman_numeral_from_arabic_value(*result, result_arabic_value);
+    convert_to_roman_numeral_from_arabic_value(result, result_arabic_value);
     return true;
   }
   else
