c7f6919eb2e39575998a81f87e69fb144b3ba8fb
Thinh Luong <tluong@pillartechnology.com>
2016-09-18 17:09:39 -0500

    Added and passed test for add_two_roman_numerals given one operand null pointer returns INVALID string.

--- tests/check_roman_numeral_operation.c
+++ tests/check_roman_numeral_operation.c
@@ -38,6 +38,17 @@ static void reset_string_variables()
   memset(actual_result, 0, strlen(actual_result));
 }
 
+START_TEST(test_add_two_roman_numerals__given_one_operand_null_pointer__returns_INVALID_string)
+{
+  void * lhs_null_pointer = NULL;
+  strcpy(rhs_roman_numeral, "I");
+
+  add_two_roman_numerals(actual_result, lhs_null_pointer, rhs_roman_numeral);
+
+  ck_assert_str_eq(failed_result, actual_result);
+}
+END_TEST
+
 START_TEST(test_add_two_roman_numerals__given_one_operand_null_pointer__returns_false)
 {
   void * lhs_null_pointer = NULL;
@@ -260,6 +271,7 @@ Suite * roman_numeral_operation_suite()
   tcase_add_checked_fixture(tcase_core, setup, teardown);
 
   tcase_set_timeout(tcase_core, 3600);
+  tcase_add_test(tcase_core, test_add_two_roman_numerals__given_one_operand_null_pointer__returns_INVALID_string);
   tcase_add_test(tcase_core, test_add_two_roman_numerals__given_one_operand_null_pointer__returns_false);
   tcase_add_test(tcase_core, test_add_two_roman_numerals__given_result_null_pointer__returns_false);
   tcase_add_test(tcase_core, test_add_two_roman_numerals__given_A_plus_I__returns_false);
